c4c629d4277b9cad4306a7ba841ca2b4e577a224
joneadoerd <joneadoerd@gmail.com>
2025-05-23 23:28:59 +0300

    fix: identifier tauri and remove unneeded components

--- src/app/SerialTab.tsx
+++ src/app/SerialTab.tsx
@@ -13,7 +13,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../components/ui/select";
-import { Badge } from "../components/ui/badge";
 import { Separator } from "../components/ui/separator";
 
 export default function SerialTab() {
